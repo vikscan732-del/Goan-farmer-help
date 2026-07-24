71b2e7b34365b49c0ae71aad25513798f298be2d
vikscan732-del <vikscan732@gmail.com>
2026-07-24 10:52:32 +0530

    dolphin_playwright.py

--- dolphin_playwright.py
+++ dolphin_playwright.py
@@ -2,21 +2,38 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
+
     page = browser.new_page()
 
-    def log_request(request):
-        print(request.method, request.url)
+    page.on(
+        "request",
+        lambda r: print(
+            "\n==== REQUEST ====",
+            "\nMETHOD:", r.method,
+            "\nURL:", r.url,
+            "\nHEADERS:", r.headers,
+            "\nPOST DATA:", r.post_data
+        )
+    )
 
-    page.on("request", log_request)
+    page.on(
+        "response",
+        lambda r: print(
+            "\n==== RESPONSE ====",
+            "\nSTATUS:", r.status,
+            "\nURL:", r.url
+        )
+    )
 
     page.goto(
-        "https://www.dolphinradar.com/web-viewer-for-instagram",
+        "https://anonyig.com/en/instagram-profile-viewer/",
         wait_until="networkidle"
     )
 
     page.locator("input").first.fill("gshclgoa")
+
     page.locator("button").first.click()
 
-    page.wait_for_timeout(10000)
+    page.wait_for_timeout(15000)
 
     browser.close()
